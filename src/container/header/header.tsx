f11193fb01479372be1e1cc78e5d7d198580858d
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-27 13:35:30 +0100

    Cleanup template

---
+++ src/container/header/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Header() {
+
+    return (
+        <header className="flex flex-row h-14 p-2.5 gap-2 text-purple-700 bg-amber-300 border-purple-700 border-b-2">
+            <Image
+                className='object-cover w-auto'
+                src='/logo.png'
+                alt='Site Logo'
+                width={50}
+                height={50}
+            />
+            <span className="text-2xl self-center hidden sm:block">
+                Demo NextJS
+            </span>
+        </header> 
+    )
+}
