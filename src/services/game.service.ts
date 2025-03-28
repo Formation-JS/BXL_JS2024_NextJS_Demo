895e80b413db56764960a5232fc83caab54c1038
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-28 10:49:45 +0100

    Add Cache
    https://nextjs.org/docs/app/api-reference/functions/unstable_cache

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,7 +1,7 @@
 import type { Game, GameRequestData } from "@/@types/game";
 import axios from "axios";
+import { unstable_cache as cache } from 'next/cache';
 
-// http://localhost:4002/games?isDeleted=false&?_page=1&_limit=3
 const apiRequester = axios.create({
     baseURL: 'http://localhost:4002',
     params: {
@@ -9,12 +9,14 @@ const apiRequester = axios.create({
     }
 });
 
-export async function fetchGames(page = 1, perPage = 3): Promise<Game[]> {
+
+export const fetchGames = cache(async (page = 1, perPage = 3): Promise<Game[]> => {
 
     //! Fausse latence... Ne pas faire ça en production (╯°□°）╯︵ ┻━┻
     await (new Promise((resolve) => setTimeout(resolve, 3_000)));
 
     //* Requete vers l'api (en NextJS, on pourrait aussi être un call DB)
+    // http://localhost:4002/games?isDeleted=false&?_page=1&_limit=3
     const result = await apiRequester.get<GameRequestData[]>('/games', {
         params: {
             _page: page,
@@ -28,4 +30,4 @@ export async function fetchGames(page = 1, perPage = 3): Promise<Game[]> {
         name: elem.name,
         genres: elem.genres.split(',').map(g => g.trim())
     }));
-}
+
